ba35165574db7d6e7bec491333e1cc0df75f0eb8
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2024-11-11 00:42:06 +0000

    :sparkles: Feat: add teste fatorial

---
+++ fatorial.test.js
@@ -0,0 +1,16 @@
+// fatorial.test.js
+const fatorial = require('./fatorial'); 
+
+describe('Função de Fatorial', () => {
+    test('Deve retornar 1 quando o número é 0', () => {
+        expect(fatorial(0)).toBe(1);
+    });
+
+    test('Deve retornar o fatorial correto para um número positivo', () => {
+        expect(fatorial(5)).toBe(120); 
+    });
+
+    test('Deve retornar uma mensagem de erro para números negativos', () => {
+        expect(fatorial(-5)).toBe("Fatorial não é definido para números negativos.");
+    });
+});
