f677c0fc7d7032ae7ad61716cf35fef331d383c7
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2024-12-03 13:02:51 +0000

    :wrench: Chore: atualizando jestconfig

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+const config = {
+    preset: 'ts-jest',
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageProvider: "v8",
+};
+export default config;
--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,15 @@
-import type {JestConfigWithTsJest} from 'ts-jest';
-
+import type { JestConfigWithTsJest } from 'ts-jest'
 const config: JestConfigWithTsJest = {
-    preset: 'ts-jest',
-    collectCoverage: true,
-    coverageDirectory: "coverage",
-    coverageProvider: "v8",
-};
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  preset: 'ts-jest/presets/default-esm',
+  testPathIgnorePatterns: ['/node_modules/'],
+  transform: {},
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  testEnvironment: 'node'
+}
 
-export default config;
+export default config
