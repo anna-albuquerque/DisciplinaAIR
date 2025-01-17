eda0fdae1c2cfd09bfd94f4c5e3a2b6d039e1780
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2025-01-17 17:56:59 +0000

    :white_check_mark: Test: eletronico.test.ts Testes

--- eletronico.test.ts
+++ eletronico.test.ts
@@ -1,20 +1,22 @@
 import { Produto } from "./produto";
 import { Eletronico, Roupa } from './eletronico';
 
-test('deve instanciar e alterar atributos de Eletronico e Roupa e testar metodo estatico', () => {
+test('deve instanciar e alterar atributos de Eletronico e Roupa e testar metodo estático', () => {
+  // Instanciar Eletronico
   const celular = new Eletronico("Smartphone", 1000, 0.15);
   Produto.mostrarPreco(celular); // O preço do produto Smartphone é R$1150.00
   expect(celular.calcularPreco()).toBe(1150);
 
-  celular.precoBase = 1200;
+  celular.precoBase = 1200; // Alterar preço base
   Produto.mostrarPreco(celular); // O preço do produto Smartphone é R$1380.00
   expect(celular.calcularPreco()).toBe(1380);
 
+  // Instanciar Roupa
   const camiseta = new Roupa("Camiseta", 50, 0.1);
   Produto.mostrarPreco(camiseta); // O preço do produto Camiseta é R$55.00
   expect(camiseta.calcularPreco()).toBe(55);
 
-  camiseta.precoBase = 70;
+  camiseta.precoBase = 70; // Alterar preço base
   Produto.mostrarPreco(camiseta); // O preço do produto Camiseta é R$77.00
   expect(camiseta.calcularPreco()).toBe(77);
 });
