1c49b0ef360177dd0c58f6f6b04e0e81e8083153
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2025-01-17 13:25:02 +0000

    :bug: Fix: corrigindo test para instanciar classes

--- classes.test.ts
+++ classes.test.ts
@@ -1,12 +1,15 @@
 import { Livro, Filme } from './classes';
 
-const livro = new Livro("Dom Casmurro", "Machado de Assis");
-const filme = new Filme("Central do Brasil", "Walter Salles");
+test('teste para instanciar e alterar atributos de Livro e Filme', () => {
+  const livro = new Livro("Dom Casmurro", "Machado de Assis");
+  expect(livro.descricao()).toBe("Dom Casmurro, escrito por Machado de Assis.");
 
-console.log(livro.descricao()); // Dom Casmurro, escrito por Machado de Assis.
-livro.titulo = "Memórias Póstumas de Brás Cubas";
-console.log(livro.descricao()); // Memórias Póstumas de Brás Cubas, escrito por Machado de Assis.
+  livro.titulo = "Memórias Póstumas de Brás Cubas";
+  expect(livro.descricao()).toBe("Memórias Póstumas de Brás Cubas, escrito por Machado de Assis.");
 
-console.log(filme.descricao()); // Central do Brasil, dirigido por Walter Salles.
-filme.titulo = "Cidade de Deus";
-console.log(filme.descricao()); // Cidade de Deus, dirigido por Walter Salles.
+  const filme = new Filme("Central do Brasil", "Walter Salles");
+  expect(filme.descricao()).toBe("Central do Brasil, dirigido por Walter Salles.");
+
+  filme.titulo = "Cidade de Deus";
+  expect(filme.descricao()).toBe("Cidade de Deus, dirigido por Walter Salles.");
+});
