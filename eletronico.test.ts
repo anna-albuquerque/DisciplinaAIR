e85e519d978531471566103311fc9372da4ab457
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2025-01-17 13:27:03 +0000

    :bug: Fix: corrigindo test para instanciar subclasses

--- eletronico.test.ts
+++ eletronico.test.ts
@@ -1,12 +1,19 @@
-import { Eletronico, Roupa } from './eletronico';
+import { Eletronico, Roupa, Produto } from './eletronico';
 
-const celular = new Eletronico("Smartphone", 1000, 0.15);
-const camiseta = new Roupa("Camiseta", 50, 0.1);
+test('deve instanciar e alterar atributos de Eletronico e Roupa e testar metodo estatico', () => {
+  const celular = new Eletronico("Smartphone", 1000, 0.15);
+  Produto.mostrarPreco(celular); // O preço do produto Smartphone é R$1150.00
+  expect(celular.calcularPreco()).toBe(1150);
 
-Produto.mostrarPreco(celular); // O preço do produto Smartphone é R$1150
-celular.precoBase = 1200;
-Produto.mostrarPreco(celular); // O preço do produto Smartphone é R$1380
+  celular.precoBase = 1200;
+  Produto.mostrarPreco(celular); // O preço do produto Smartphone é R$1380.00
+  expect(celular.calcularPreco()).toBe(1380);
 
-Produto.mostrarPreco(camiseta); // O preço do produto Camiseta é R$45
-camiseta.precoBase = 70;
-Produto.mostrarPreco(camiseta); // O preço do produto Camiseta é R$63
+  const camiseta = new Roupa("Camiseta", 50, 0.1);
+  Produto.mostrarPreco(camiseta); // O preço do produto Camiseta é R$55.00
+  expect(camiseta.calcularPreco()).toBe(55);
+
+  camiseta.precoBase = 70;
+  Produto.mostrarPreco(camiseta); // O preço do produto Camiseta é R$77.00
+  expect(camiseta.calcularPreco()).toBe(77);
+});
