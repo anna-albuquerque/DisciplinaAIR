2ef0b0ebdda4cbb9e5b2ae976215b3fc6024169a
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2025-01-17 18:02:18 +0000

    :wrench: Chore: Atividade Parte 2

--- eletronico.ts
+++ eletronico.ts
@@ -1,23 +1,23 @@
 import { Produto } from './produto';
 
 class Eletronico extends Produto {
-  constructor(nome: string, precoBase: number, public taxa: number) {
-    super(nome, precoBase);
+    constructor(nome: string, precoBase: number, public taxa: number) {
+      super(nome, precoBase);
+    }
+  
+    calcularPreco(): number {
+      return parseFloat((this.precoBase * (1 + this.taxa)).toFixed(2)); // Arredonda para 2 casas decimais
+    }
   }
-
-  calcularPreco(): number {
-    return this.precoBase * (1 + this.taxa); // Aplica a taxa
+  
+  class Roupa extends Produto {
+    constructor(nome: string, precoBase: number, public taxa: number) {
+      super(nome, precoBase);
+    }
+  
+    calcularPreco(): number {
+      return parseFloat((this.precoBase * (1 + this.taxa)).toFixed(2)); // Arredonda para 2 casas decimais
+    }
   }
-}
-
-class Roupa extends Produto {
-  constructor(nome: string, precoBase: number, public taxa: number) {
-    super(nome, precoBase);
-  }
-
-  calcularPreco(): number {
-    return this.precoBase * (1 + this.taxa); // Aplica a taxa
-  }
-}
-
-export { Eletronico, Roupa };
+  
+  export { Eletronico, Roupa };
